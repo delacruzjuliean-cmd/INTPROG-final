6a7f6213f82576de18a02244543564e519596cae
Juliean Dela Cruz <delacruzjuliean_bsit@plmun.edu.ph>
2025-10-19 12:54:40 +0800

    Add files via upload

---
+++ lab1.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: application/json');
+
+$students = array(
+    array("Name" => "Juliean", "Age" => 20, "Course" => "Information Technology"),
+    array("Name" => "Bryan", "Age" => 22, "Course" => "Information Technology"),
+    array("Name" => "Elysa", "Age" => 21, "Course" => "Information Technology")
+);
+
+// Convert to pretty JSON
+$json = json_encode($students, JSON_PRETTY_PRINT);
+
+// Output formatted JSON
+echo $json;
+?>
