0de505d59f61aaeffc6921c38483f0127f24dff3
Juliean Dela Cruz <delacruzjuliean_bsit@plmun.edu.ph>
2025-10-19 13:16:23 +0800

    Update lab1.php

--- lab1.php
+++ lab1.php
@@ -13,3 +13,5 @@ $json = json_encode($students, JSON_PRETTY_PRINT);
 // Output formatted JSON
 echo $json;
 ?>
+
+*[Exercise 1 - HTML/CSS](https://drive.google.com/drive/folders/1KoZj4kEB_mMgE3f3-opu56Tw-R6yHaRS?usp=drive_link)
